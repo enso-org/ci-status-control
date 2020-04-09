1433c9dc689983cf48e0210b563fe42d9611797c
Ara Adkins <me@ara.io>
2020-04-09 14:57:54 +0100

    Testing file lists 5

--- index.js
+++ index.js
@@ -11,39 +11,49 @@ function processPushEvent(github, octokit, event, excludedPaths, stopInternally)
 function processPREvent(github, octokit, event, excludedPaths, stopInternally) {
 }
 
-try {
-    const githubToken = core.getInput('github-token');
-    const octokit = new github.GitHub(githubToken);
+async function run() {
+    try {
+        const githubToken = core.getInput('github-token');
+        const octokit = new github.GitHub(githubToken);
 
-    // `who-to-greet` input defined in action metadata file
-    const excludedPaths = core.getInput('excluded-paths').split('\n');
-    const stopInternally = core.getInput('stop-internally');
+        // `who-to-greet` input defined in action metadata file
+        const excludedPaths = core.getInput('excluded-paths').split('\n');
+        const stopInternally = core.getInput('stop-internally');
 
-    console.log(github.context.sha);
+        console.log(github.context.sha);
 
-    // We only care about `pull_request` and `push` events as they're the only
-    // ones that can change commit messages or files
-    const eventName = github.context.eventName;
-    const event = github.event;
+        // We only care about `pull_request` and `push` events as they're the only
+        // ones that can change commit messages or files
+        const eventName = github.context.eventName;
+        const event = github.event;
 
-    if (eventName == 'push' || eventName == 'pull_request') {
-        const mergeCommitSha = github.context.sha;
-        const repoName = github.context.repoName;
-        const repoOwner = github.context.repoOwner;
+        if (eventName == 'push' || eventName == 'pull_request') {
+            const mergeCommitSha = github.context.sha;
+            const repoName = github.context.repoName;
+            const repoOwner = github.context.repoOwner;
 
-        console.log(mergeCommitSha);
+            console.log(mergeCommitSha);
 
-        // octokit.git.getCommit({repoOwner, repoName, mergeCommitSha})
-            // .then((commit) => {
-                // console.log(commit.files);
-            // });
-    } else {
-        core.setOutput('stop-code', 'nothing')
+            const commit = await octokit.git.getCommit({
+                repoOwner,
+                repoName,
+                mergeCommitSha
+            });
+
+            // octokit.git.getCommit({repoOwner, repoName, mergeCommitSha})
+                // .then((commit) => {
+                    // console.log(commit.files);
+                // });
+        } else {
+            core.setOutput('stop-code', 'nothing')
+        }
+
+        // const payload = JSON.stringify(github.context.payload, undefined, 2)
+        // console.log(`The event payload: ${payload}`);
+    } catch (error) {
+        core.setFailed(error.message);
     }
-
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
 }
+
+run();
 
